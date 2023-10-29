c890d4622e98f093562ace8f6add263a9b146cf6
JuanM0412 <juangomezp1204@gmail.com>
2023-10-29 12:00:23 -0500

    Migrations

--- DjangoP/bias/Offer/admin.py
+++ DjangoP/bias/Offer/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+admin.site.register(Offer)
+admin.site.register(Suggestion)
+admin.site.register(Report)
+admin.site.register(Bias)
